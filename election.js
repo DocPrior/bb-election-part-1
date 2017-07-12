485ba141c12c03064ad96ebedd9fb46a54c8dc44
DocPrior <Sara Prior>
2017-07-12 14:29:37 -0400

    finish part 1

--- election.js
+++ election.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+var votes = document.querySelector('button');
+var list = document.querySelector('#results_data');
+votes.addEventListener('click', function(){
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    datatype: 'json'
+  }).done(function(responseData) {
 
-  // Imagination!
+      for (i = 0; i < responseData.candidates.length; i++){
+        var listItem = document.createElement('li')
+        listItem.innerHTML = 'Name: ' + responseData.candidates[i].name + ' ' + 'Votes: ' + responseData.candidates[i].votes;
+        list.append(listItem);
+      }
+  });
+})
+
 
 });
